c2dbff12eb34857135f8aac24bb8e20bff673065
Newt vo <nhutmvo1997@gmail.com>
2020-09-08 00:16:04 -0400

    die method for Cat

--- Cat.js
+++ Cat.js
@@ -1,9 +1,14 @@
 function Cat() {
     this.stomach = [];
+    this.isDead = false;
 }
 
 Cat.prototype.eat = function(mouse) {
     this.stomach.push(mouse);
+}
+
+Cat.prototype.die = function() {
+    this.isDead = true;
 }
 
 module.exports = Cat;
